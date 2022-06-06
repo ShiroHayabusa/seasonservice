eceffd3b4a449b2e605e6bec805544fa845282de
shirohayabbbusa <shirohayabbbusa@gmail.com>
2022-06-07 05:24:09 +0700

    добавил шифрование пароля в sql-скрипте

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,5 @@ values (1, 'ROLE_ADMIN'), (2, 'ROLE_USER');
 insert into users (id, login, password, first_name, last_name, patronymic, email, role_id)
 values (1, 'admin', 'pass', 'admin', 'admin', 'admin', 'null', 1);
 
+create extension if not exists pgcrypto;
+update users set password = crypt(password, gen_salt('bf', 8));
